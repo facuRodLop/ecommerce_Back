2577378661c6559a8200079b979de7affecfe5ea
Mauro Montero <93015917+canutoCanuto@users.noreply.github.com>
2022-03-29 14:41:00 -0300

    try&catch en crud de usuarios y productos

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,42 +2,67 @@ const { Product } = require("../models");
 
 // Display a listing of the resource.
 async function index(req, res) {
-  const products = await Product.findAll({ nested: true });
-  res.status(202).json(products);
+  try {
+    const products = await Product.findAll({ nested: true });
+    res.status(202).json(products);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
 // Display the specified resource.
 async function show(req, res) {
-  const product = await Product.findOne({ where: { slug: req.params.id } });
-  res.status(202).json(product);
+  try {
+    const product = await Product.findOne({ where: { slug: req.params.id } });
+    res.status(202).json(product);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
 // Store a newly created resource in storage.
 async function store(req, res) {
-  await Product.create(req.body);
-  res.status(201).json({ message: "The Product was successfully created" });
+  try {
+    await Product.create(req.body);
+    res.status(201).json({ message: "The Product was successfully created" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
 // Update the specified resource in storage.
 async function update(req, res) {
-  await Product.update(req.body, { where: { id: req.params.id } });
-  res.status(206).json({
-    message: "The Product was successfully updated",
-  });
+  try {
+    await Product.update(req.body, { where: { id: req.params.id } });
+    res.status(206).json({
+      message: "The Product was successfully updated",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: error.message });
+  }
 }
 
 async function sold(req, res) {
-  const { quantitySold } = req.body;
-  const { stock } = sdf;
-  const remainder = stock - quantitySold;
-  await Product.update({ stock: amount }, { where: { id: req.params.id } });
-  res.status(202).json({ message: "The Product was successfully sold" });
+  try {
+    const { quantitySold } = req.body;
+    const { stock } = sdf;
+    const remainder = stock - quantitySold;
+    await Product.update({ stock: amount }, { where: { id: req.params.id } });
+    res.status(202).json({ message: "The Product was successfully sold" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
 // Remove the specified resource from storage.
 async function destroy(req, res) {
-  await User.destroy({ where: { id: req.params.id } });
-  res.status(200).json({ message: "The User was deleted successfully" });
+  try {
+    await User.destroy({ where: { id: req.params.id } });
+    res.status(200).json({ message: "The User was deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
 module.exports = {
--- controllers/userController.js
+++ controllers/userController.js
@@ -4,37 +4,57 @@ const bcrypt = require("bcryptjs");
 
 // Display a listing of the resource.
 async function index(req, res) {
-  const users = await User.findAll({ nested: true });
-  res.status(202).json(users);
+  try {
+    const users = await User.findAll({ nested: true });
+    res.status(202).json(users);
+  } catch (error) {
+    res.status(400).json({ message: "Something went wrong" });
+  }
 }
 
 // Display the specified resource.
 async function show(req, res) {
-  const { id } = req.params;
-  const user = await User.findByPk(id);
-  res.status(202).json(user);
+  try {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    res.status(202).json(user);
+  } catch (error) {
+    res.status(400).json({ message: "Something went wrong" });
+  }
 }
 
 // Store a newly created resource in storage.
 async function store(req, res) {
-  await User.create({ ...req.body, isAdmin: false });
-  res.status(201).json({ message: "The User was successfully created" });
+  try {
+    await User.create({ ...req.body, isAdmin: false });
+    res.status(201).json({ message: "The User was successfully created" });
+  } catch (error) {
+    res.status(400).json({ message: "Something went wrong" });
+  }
 }
 
 // Update the specified resource in storage.
 async function update(req, res) {
-  const { id } = req.params;
-  await User.update(req.body, { where: { id: id } });
-  res.status(206).json({
-    message: "The User was successfully updated",
-  });
+  try {
+    const { id } = req.params;
+    await User.update(req.body, { where: { id: id } });
+    res.status(206).json({
+      message: "The User was successfully updated",
+    });
+  } catch (error) {
+    res.status(400).json({ message: "Something went wrong" });
+  }
 }
 
 // Remove the specified resource from storage.
 async function destroy(req, res) {
-  const { id } = req.params;
-  await User.destroy({ where: { id: id } });
-  res.status(200).json({ message: "The User was deleted successfully" });
+  try {
+    const { id } = req.params;
+    await User.destroy({ where: { id: id } });
+    res.status(200).json({ message: "The User was deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ message: "Something went wrong" });
+  }
 }
 
 // Login & logout con jsonwebtoken
