5728dc97ffff812453d4d6719a431c3f51526ec4
Luis Alvarez <96274912+Lraq1991@users.noreply.github.com>
2022-04-06 04:58:58 -0300

    Loading Images

--- controllers/productController.js
+++ controllers/productController.js
@@ -40,17 +40,13 @@ async function store(req, res) {
 }
 //Store file with formidable.
 async function storeFile(req, res) {
-  const supabase = createClient(
-    "https://ufonbmlolgrovmofzijx.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmb25ibWxvbGdyb3Ztb2Z6aWp4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0OTA4OTA4MywiZXhwIjoxOTY0NjY1MDgzfQ.FmaDrt7e5NkwKJRbPjlX1tWEcg30peyryt7p7rgxwE0",
-  );
+  const supabase = createClient(process.env.DB_BASE_URL, process.env.ANON_KEY);
   const form = formidable({
     multiples: true,
     keepExtensions: true,
   });
   form.parse(req, async (err, fields, files) => {
     console.log("files", files);
-    //
     const { data, error } = await supabase.storage
       .from("images")
       .upload(files.image.newFilename, fs.createReadStream(files.image.filepath), {
@@ -58,29 +54,8 @@ async function storeFile(req, res) {
         upsert: false,
         contentType: files.image.mimetype,
       });
+    res.json({ message: files.image.newFilename });
   });
-  // const user = await User.findOne({ where: { id: req.user.sub } });
-
-  // if (user.isAdmin) {
-  //   try {
-  //     const form = formidable.IncomingForm();
-  //     console.log("esta en try");
-  //     form
-  //       .parse(req, (err, fields, files) => {})
-  //       .on("fileBegin", (name, file) => {
-  //         console.log("entro en fileBegin");
-  //         file.path = "/home/luis/Documents/GitHub/ecommerce_Back/public/img" + file.name;
-  //       })
-  //       .on("file", (file) => {
-  //         console.log("Uploaded!");
-  //       });
-  //     res.status(201).json({ message: "The File was successfully stored!" });
-  //   } catch (error) {
-  //     res.status(400).json({ message: error.message });
-  //   }
-  // } else {
-  //   res.status(401).json({ message: "Unauthorized" });
-  // }
 }
 
 // Update the specified resource in storage.
